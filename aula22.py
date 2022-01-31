9ac56cfe966a2a402e0e47b3030ffae9d0c9f8e7
gusta <gustavo_o_@hotmail.com>
2022-01-31 19:23:03 -0400

    Duck Types

---
+++ aula22.py
@@ -0,0 +1,13 @@
+class CisneiNegro:
+    def __len__(self):
+        return 20
+
+
+livro = CisneiNegro()
+print(len(livro))
+nome = "Gustavo Oliveira"
+lista = [1, 2, 3, 4]
+dicio = {"eu": 1, "tu": 2, "ele": 3}
+print(len(nome))
+print(len(lista))
+print(len(dicio))
